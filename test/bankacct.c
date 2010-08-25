f58f696b85ded4fb5639d4d873dcbebae13b8dd8
heechul.yun <heechul.yun@904cbf56-2e09-8dc9-4bc8-b31f8fb44dec>
2010-08-25 15:47:46 +0000

    Add bank account example

---
+++ test/bankacct.c
@@ -0,0 +1,126 @@
+/**
+ * A classical bank account example that demonstrate atomicity violation. 
+ *
+ * Author: Heechul Yun <heechul@illinois.edu> 
+ *
+ * This file is distributed under the University of Illinois Open Source
+ * License. See LICENSE.TXT for details.
+ *
+ */ 
+#include <stdio.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <stdlib.h>
+#include <err.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <assert.h>
+
+// NOTE: uncomment the following line to make app deterministic 
+//#include <dpthread-wrapper.h>
+
+// defines 
+#define DBG(x) x 
+
+// glocal variables 
+static volatile int g_account;  // account 
+
+static pthread_mutex_t lock; 
+
+// local functions 
+unsigned long fib(unsigned long n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 2;
+	return fib(n-1)+fib(n-2);
+}
+
+// worker definitions 
+int Read()
+{
+}
+
+void *
+Deposit(void *v)
+{
+	int balance = g_account; // ERROR: balance can be stale
+	int amount = (int)v; 
+
+	pthread_mutex_lock(&lock);
+	g_account = balance + amount; 
+	pthread_mutex_unlock(&lock); 
+
+	return NULL; 
+}
+
+void *
+Withdraw(void *v)
+{
+	int balance = g_account; // ERROR: balance can be stale
+	int amount = (int)v; 
+
+	pthread_mutex_lock(&lock);
+	g_account = balance - amount; 
+	pthread_mutex_unlock(&lock); 
+	return NULL; 
+}
+
+
+// main 
+static void
+usage(char *argv[])
+{
+	printf("%s -x <x> -y <y>\n"
+	       "-x : input for thread 1\n" 
+	       "-y : input for thread 2\n", argv[0]); 
+}
+
+int main(int argc, char *argv[])
+{
+	pthread_t allthr[10];
+	int i, ret; 
+	int x, y; 
+
+	// initialize 
+	x = 13; 
+	y = 0; 
+
+	// parameters 
+	while((i=getopt(argc, argv, "x:y:h")) != EOF) {
+		switch(i) {
+		case 'h':
+			usage(argv);
+			return 0;
+		case 'x':
+			x = strtol(optarg, NULL, 0); 
+			break; 
+		case 'y': 
+			y = strtol(optarg, NULL, 0); 
+			break; 
+		default:
+			errx(1, "invalid option");
+		}
+	}
+
+	// lock initialize 
+	pthread_mutex_init(&lock, NULL);
+
+	g_account = 10; 
+
+	// thread create 
+	ret = pthread_create(&allthr[0], NULL, Deposit, (void *)1);
+	if (ret) err(1, "pthread_create failed");
+	
+	Withdraw((void *)2); 
+
+	// wait for workers to finish. 
+	pthread_join(allthr[0], NULL);
+
+	assert( g_account == 9 ); 
+
+	return 0; 
+}
+
